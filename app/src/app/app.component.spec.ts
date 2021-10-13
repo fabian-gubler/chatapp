abb90b89414554379dc74d20a1c15d14647e8f0d
universitychat <fabian.gubler@protonmail.com>
2021-10-13 11:55:26 +0200

    update

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'code'`, () => {
+  it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('code');
+    expect(app.title).toEqual('app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('code app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
   });
 });
