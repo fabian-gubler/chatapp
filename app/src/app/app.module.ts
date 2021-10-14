d479ef0a44c1ea4e02cf3944bf2f906a5e07c3ee
klap001 <martin.klapper@empolis.com>
2021-10-14 18:04:39 +0200

    Tester Commit

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      HeaderComponent
+   ],
   imports: [
     BrowserModule
   ],
