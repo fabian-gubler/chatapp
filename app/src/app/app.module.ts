a2a710d5a7f1c8da69908ffd289c400448fe4b56
calvinkochunisg <calvin.koch@student.unisg.ch>
2021-10-16 12:21:50 +0200

    content component erstellt

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
       HeaderComponent,
-      FooterComponent
+      FooterComponent,
+      ContentComponent
    ],
   imports: [
     BrowserModule
