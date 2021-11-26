b9a15ff2baf0e44d8f65816d89c19bc78882bc3e
klap001 <martin.klapper@empolis.com>
2021-11-26 17:08:45 +0100

    empty check update

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -12,13 +12,14 @@ export class MessageComponent {
 
   public post_message(message: string): void {
     
+
     /* test for empty (empty or newline because enter; maybe profanity filter*/
-    if (message === '') {
+    if (message === '' || message.startsWith('\n')) {
       return
     }
 
     console.log(message)
-    
+
     this.chatMessage = ''
 
 
