4f927537c389647aa0326d87f8c0f2b73bcf89e4
fabian-gubler <fabian.gubler@hotmail.com>
2021-11-27 19:41:29 +0100

    update

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -14,22 +14,39 @@ export class MessageComponent {
   @Output() public submitMessage = new EventEmitter<string>();
 
   public chatMessage = '';
+	public errorMessage = '';
+	public validMessage = true;
+
 
   public post_message(message: string, nickname: string): void {
-    
-    /* test for empty (empty or newline because enter; maybe profanity filter -- Problem : Closing Error with Enter prompts another error because of keyup event*/
-    if (message === '' || message.startsWith('\n')) {
-      window.alert("Du kannst keine leeren Nachrichten abschicken");
 
-      this.chatMessage = '';
-      return
-    }
-    
+		switch(true) {
+			// No Nickname
+			case !nickname:
+				this.errorMessage = 'Bitte erstelle einen Nickname bevor du eine Nachricht schreibst';
+				break;
+
+			// No Message
+			case !message.trim():
+				this.errorMessage = 'Du kannst keine leeren Nachrichten abschicken';
+				break;
+
+			// Needed to check validity
+			default:
+				this.errorMessage = ''
+		}
+
+		// Chat Output
     const timestamp: string = new Date().toLocaleString('de');
     const finalmessage = `${nickname} : ${message} - ${timestamp} <br>`;
 
+		// Push only if valid
+		if (this.errorMessage == '') {
+			console.log("sucess")
+			this.submitMessage.emit(finalmessage);
+		}
 
-    this.submitMessage.emit(finalmessage);
+		// Initialize variable
     this.chatMessage = '';
 
     return
--- src/app/nickname/nickname.component.ts
+++ src/app/nickname/nickname.component.ts
@@ -11,13 +11,30 @@ export class NicknameComponent implements OnInit {
 
   public init_nickname = ''
   public savemsg = ''
+	public errorMessage = ''
 
   public save_nickname(nickname: string): void {
+
+		// Empty Nickname
+		if (!nickname) {
+			this.errorMessage = 'Bitte erstelle Einen Nicknamen';
+			return
+		}
+		// Check whether multiple words
+		if (nickname.includes(' ')) {
+			this.errorMessage = 'Dein Nickname soll keine Leerzeichen beinhalten';
+      return
+		}
+
+		// Maximum character length
+		if (nickname.length > 15) {
+			this.errorMessage = 'Dein Nickname darf maximal 15 Zeichen haben';
+		}
+
+		// Submit Nickname
     this.submitNickname.emit(nickname);
 
     this.savemsg = 'Nickname: ' + nickname + ' gespeichert'
-
-    window.alert(this.savemsg)
 
     return
 
